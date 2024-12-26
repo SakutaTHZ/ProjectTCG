3b8adff41d787682375d73d3adc8dcaff337a1e3
Thar Htet Zan <110976118+SakutaTHZ@users.noreply.github.com>
2024-12-26 23:16:10 +0630

    Added structure

--- ProjectT/src/components/CardContainer.jsx
+++ ProjectT/src/components/CardContainer.jsx
@@ -1,16 +1,19 @@
 import PropTypes from "prop-types";
 
-const CardContainer = ({ customClass = "" ,style=''}) => {
+const CardContainer = ({ customClass = "", style = {} }) => {
   return (
     <>
-      <div className={`w-24 h-36 rounded-md border-dotted border-2 backdrop-blur-lg transition-all ease-[cubic-bezier(0.6, -0.28, 0.735, 0.045)] delay-100 ${customClass}`} style={style}></div>
+      <div
+        className={`w-24 h-36 rounded-md border-dotted border-2 border-opacity-45 backdrop-blur-lg transition-all ease-[cubic-bezier(0.6, -0.28, 0.735, 0.045)] delay-200 ${customClass}`}
+        style={style}
+      ></div>
     </>
   );
 };
 
 CardContainer.propTypes = {
   customClass: PropTypes.string,
-  style: PropTypes.string,
+  style: PropTypes.object, // Expecting an object instead of a string
 };
 
 export default CardContainer;
--- ProjectT/src/components/PlayerDeckContainer.jsx
+++ ProjectT/src/components/PlayerDeckContainer.jsx
@@ -7,48 +7,83 @@ const PlayerDeckContainer = ({ customClass = "" }) => {
   const [rotationAngle, setRotationAngle] = React.useState(0);
 
   const rotate = () => {
-    setRotationAngle(rotationAngle + 120); // Rotate 120 degrees for each click
+    setRotationAngle(rotationAngle + 120);
   };
+
+  const cards = Array.from({ length: 10 });
 
   return (
     <>
       <div
-        className={`grid grid-cols-3 gap-5 min-h-32 min-w-96 rounded border-dotted border-2 p-2 ${customClass}`}
+        className={`flex justify-center gap-20 min-h-32 w-screen rounded p-2 items-center ${customClass}`}
       >
-        <div
-          className="characters w-52 relative border flex justify-center items-center aspect-square rounded-full transition-transform duration-500"
-          style={{ transform: `rotate(${rotationAngle}deg)` }}onClick={rotate}
-        >
-          {/* Top Card */}
-          <CardContainer
-            customClass={`absolute transform -translate-x-1/2 -translate-y-full`}
-            style={{
-              top: "-35%",
-              transform: `rotate(-${rotationAngle}deg)`,
-            }}
-          />
+        {/* First Column - Rotating Characters */}
+        <div className="flex w-[15%] justify-center items-center">
+          <div
+            className="characters h-52 w-fit relative border flex justify-center items-center aspect-square rounded-full transition-transform duration-500"
+            style={{ transform: `rotate(${rotationAngle}deg)` }}
+            onClick={rotate}
+          >
+            {/* Top Card */}
+            <CardContainer
+              customClass={`absolute transform -translate-x-1/2 -translate-y-full`}
+              style={{
+                top: "-35%",
+                transform: `rotate(-${rotationAngle}deg)`,
+              }}
+            />
 
-          {/* Bottom-Right Card */}
-          <CardContainer
-            customClass={`absolute transform -translate-x-1/2 translate-y-1/2`}
-            style={{
-              bottom: "-10%",
-              left: "-17%", // Adjusted for wider spacing
-              transform: `rotate(-${rotationAngle}deg)`,
-            }}
-          />
+            {/* Bottom-Right Card */}
+            <CardContainer
+              customClass={`absolute transform -translate-x-1/2 translate-y-1/2`}
+              style={{
+                bottom: "-10%",
+                left: "-17%", // Adjusted for wider spacing
+                transform: `rotate(-${rotationAngle}deg)`,
+              }}
+            />
 
-          {/* Bottom-Left Card */}
-          <CardContainer
-            customClass={`absolute transform -translate-x-1/2 translate-y-1/2`}
-            style={{
-              bottom: "-10%",
-              right: "-17%", // Adjusted for wider spacing
-              transform: `rotate(-${rotationAngle}deg)`,
-            }}
-          />
+            {/* Bottom-Left Card */}
+            <CardContainer
+              customClass={`absolute transform -translate-x-1/2 translate-y-1/2`}
+              style={{
+                bottom: "-10%",
+                right: "-17%", // Adjusted for wider spacing
+                transform: `rotate(-${rotationAngle}deg)`,
+              }}
+            />
+          </div>
         </div>
 
+        {/* Second Column - Spell and Draft Cards */}
+        <div className="flex flex-col border px-4 py-4 gap-4 w-[60%] justify-center">
+          {/* Spell Cards */}
+          <div className="flex justify-center">
+            <div className="spellCards flex gap-2 justify-center">
+              <CardContainer customClass={`border-yellow-500`} />
+              <CardContainer customClass={`border-yellow-500`} />
+              <CardContainer customClass={`border-yellow-500`} />
+            </div>
+          </div>
+
+          {/* Draft Cards - Fan Layout */}
+          <div className="draftCards min-h-36 flex justify-center relative">
+            {cards.map((_, index) => {
+              return (
+                <CardContainer
+                  key={index}
+                  customClass="border-blue-500 flex-shrink-0"
+                />
+              );
+            })}
+          </div>
+        </div>
+
+        
+        <div className="flex flex-col gap-4">
+              <CardContainer customClass={`border-green-500 card-pile`} />
+              <CardContainer customClass={`border-red-500 card-pile`} />
+            </div>
       </div>
     </>
   );
